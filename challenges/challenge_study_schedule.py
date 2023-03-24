aaa7441b741733038501ddf93ef8fd7d9701d436
p4n1k0 <gabre.tarick@gmail.com>
2023-03-24 18:58:13 -0300

    identa

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,6 +1,6 @@
 def study_schedule(permanence_period, target_time):
-        count = 0
-        for period in permanence_period:
-            if target_time >= period[0] and target_time <= period[1]:
-                count += 1
-        return count
+    count = 0
+    for period in permanence_period:
+        if target_time >= period[0] and target_time <= period[1]:
+            count += 1
+    return count
