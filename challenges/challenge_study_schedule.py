8bb7c51a19e5ee99281c0df15c3353d951378052
p4n1k0 <gabre.tarick@gmail.com>
2023-03-24 19:28:18 -0300

    desisto de comentar

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,9 +4,6 @@ def study_schedule(permanence_period, target_time):
         for period in permanence_period:
             if target_time >= period[0] and target_time <= period[1]:
                 count += 1
-#Retorna para uma entrada específica a quantidade de estudantes presentes
         return count
     except Exception:
-#None se permanence_period houver alguma entrada inválida e se target_time
-# recebe valor vazio
         None
