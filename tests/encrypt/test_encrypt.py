ee4ddd0bb7eebda72656b96dd34f4a957fc673c5
p4n1k0 <gabre.tarick@gmail.com>
2023-03-25 18:06:56 -0300

    testes de acerto e rejeicao de implementacoes

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,11 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message('testando', 8) == 'odnatset'
+    assert encrypt_message('testando', 4) == 'odna_tset'
+    with pytest.raises(TypeError):
+        encrypt_message('teste de função', '')
+    with pytest.raises(TypeError):
+        encrypt_message(1, 1)
