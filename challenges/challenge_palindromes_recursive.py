073a07b57920e5caff45ac2143c528f3a4cd17f9
p4n1k0 <gabre.tarick@gmail.com>
2023-03-26 10:51:19 -0300

    True se word = palindromo, False senao for e False se nao for passada nenhum word

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if low_index >= high_index:
+        return True
+    if not word or word[low_index] != word[high_index]:
+        return False
+    else:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
