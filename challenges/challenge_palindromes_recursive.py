6f6b179926b77b379d85014f68cde9f9e0cd12ee
p4n1k0 <gabre.tarick@gmail.com>
2023-03-26 11:02:22 -0300

    fix False se nenhuma palavra for passada

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,8 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """Faça o código aqui."""
-    if low_index >= high_index:
-        return True
-    if not word or word[low_index] != word[high_index]:
+    if not word:
         return False
+    elif word[low_index] != word[high_index]:
+        return False
+    elif low_index >= high_index:
+        return True
     else:
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
