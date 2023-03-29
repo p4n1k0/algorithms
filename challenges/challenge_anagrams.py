b68215847c9f0e032e75e2cf7fd2bd82226fc7ff
p4n1k0 <gabre.tarick@gmail.com>
2023-03-29 12:35:52 -0300

    True se words forem anagramas, False senao. False se algumas words sao str + complexidade assinc linearitmica + true se words forem anagramas

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,20 @@
-def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def merge_string(first_str, second_str, index_str=0, index_str2=0, merged_str=''):
+    while index_str < len(first_str) and index_str2 < len(second_str):
+        if first_str[index_str] < second_str[index_str2]:
+            merged_str += first_str[index_str]
+            index_str += 1
+        else:
+            merged_str += second_str[index_str2]
+            index_str2 += 1
+    return merged_str + first_str[index_str:] + second_str[index_str2:]
+
+def sort_string(str):
+    if len(str) <= 1:
+        return str
+    return merge_string(sort_string(str[:len(str) // 2]), sort_string(str[len(str) // 2:]))
+
+def is_anagram(first_str, second_str):
+    first_str = sort_string(first_str.lower())
+    second_str = sort_string(second_str.lower())
+    is_anagram = first_str, second_str, first_str != '' and first_str == second_str
+    return is_anagram
