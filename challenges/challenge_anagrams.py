e2430dd70c54a80e109ee4898f2f925a8c67bb10
p4n1k0 <gabre.tarick@gmail.com>
2023-03-30 15:58:52 -0300

    passou?

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,5 +1,5 @@
-def merge_string(first_str, second_str, index_str=0, index_str2=0):
-    merged_str = ''
+def merge_string(first_str, second_str, index_str=0, index_str2=0,
+                 merged_str=''):
     while index_str < len(first_str) and index_str2 < len(second_str):
         if first_str[index_str] < second_str[index_str2]:
             merged_str += first_str[index_str]
@@ -12,7 +12,7 @@ def merge_string(first_str, second_str, index_str=0, index_str2=0):
 
 def sort_string(str):
     if len(str) <= 1:
-        return str 
+        return str
     return merge_string(sort_string(str[:len(str) // 2]),
                         sort_string(str[len(str) // 2:]))
 
@@ -20,5 +20,6 @@ def sort_string(str):
 def is_anagram(first_str, second_str):
     first_str = sort_string(first_str.lower())
     second_str = sort_string(second_str.lower())
-    is_anagram = first_str, second_str, first_str != '' and first_str == second_str
+    is_anagram = first_str, second_str,
+    first_str != '' and first_str == second_str
     return is_anagram
