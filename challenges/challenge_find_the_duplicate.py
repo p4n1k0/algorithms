9f951addadbf24e4ae26d3e55a6dfe60ac7ddf12
p4n1k0 <gabre.tarick@gmail.com>
2023-04-17 13:51:18 -0300

    num repetitivo if func receber lista com num repetidos

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,10 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) <= 1:
+        False
+    nums.sort()
+    for index in range(len(nums) - 1):
+        if not isinstance(nums[index], int) or nums[index] < 0:
+            return False
+        if nums[index] == nums[index + 1]:
+            return nums[index]
+    return False
